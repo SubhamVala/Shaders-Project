53d0bbbcdfcc57641ff2569e0169fbfe70d2b70b
P2853383 <P2853383@GWHXX632WIN008W>
2025-11-03 11:28:30 +0000

    added another cube where it rotates

--- Application_Clean/src/myScene.cpp
+++ Application_Clean/src/myScene.cpp
@@ -14,26 +14,26 @@ myScene::~myScene()
 
 void myScene::MakeVAO() 
 {
-	glCreateBuffers(1, &VBO);
+	glCreateBuffers(1, &VBO); // creates vertex buffer object
 	glNamedBufferStorage(VBO, sizeof(float) * vertexData.size(), vertexData.data(), GL_DYNAMIC_STORAGE_BIT);
 	
-	glCreateBuffers(1, &EBO);
+	glCreateBuffers(1, &EBO); // creates an element buffer object.
 	glNamedBufferStorage(EBO, sizeof(unsigned int) * cubeIndices.size(), cubeIndices.data(), GL_DYNAMIC_STORAGE_BIT);
 
 
-	glCreateVertexArrays(1, &VAO);
+	glCreateVertexArrays(1, &VAO); // creates vertex array object.
 	glVertexArrayVertexBuffer(VAO, 0, VBO, 0, sizeof(float) * 3);
-	glVertexArrayElementBuffer(VAO, EBO);
+	glVertexArrayElementBuffer(VAO, EBO); // Adding EBO TO VBO
 
 	glEnableVertexArrayAttrib(VAO, 0);
-	/*glEnableVertexArrayAttrib(VAO, 1);*/
+
 
 
 	glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
-	/*glVertexArrayAttribFormat(VAO, 1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float));*/
+
 
 	glVertexArrayAttribBinding(VAO, 0, 0);
-	/*glVertexArrayAttribBinding(VAO, 1, 0);*/
+
 }
 
 void myScene::update(float dt) {
@@ -43,26 +43,32 @@ void myScene::update(float dt) {
 
 void myScene::render() 
 {
-	m_model = glm::mat4(1.0f);
-
+	m_model = glm::mat4(1.0f); // identity matrix
 	//camera
-
 	m_projection = m_camera->getProjectionMatrix();
 	m_view = m_camera->getViewMatrix();
 	my_shader->use();
 
-	//uniforms
+	//set uniforms
+	my_shader->setMat4("Model", m_model);
 	my_shader->setMat4("View", m_view);
 	my_shader->setMat4("Projection", m_projection);
-	my_shader->setMat4("Model", m_model);
-	
-	
+
+	m_model = glm::translate(m_model, glm::vec3(5, 0.0, 0.0));
+
+	m_model = glm::rotate(m_model, (float)(glfwGetTime() * 3), glm::vec3(2.0, 0.0, 2.0));
+
+	m_model = glm::scale(m_model, glm::vec3(1.5, 1.5, 1.5));
 
 	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, cubeIndices.size(), GL_UNSIGNED_INT, 0) ;
-	//glDrawArrays(GL_TRIANGLES, 0, 6);
+	glDrawElements(GL_TRIANGLES, cubeIndices.size(), GL_UNSIGNED_INT, 0);
 
 	
 	
+	my_shader->setMat4("Model", m_model);
+	//draw call for cube 2.
+	glDrawElements(GL_TRIANGLES, vertexData.size(), GL_UNSIGNED_INT, 0);
+
 	
+
 }
--- Shaders/vertexshader.glsl
+++ Shaders/vertexshader.glsl
@@ -1,7 +1,6 @@
 #version 460 core
 
 layout(location = 0) in vec3 aPos;
-//layout(location = 1) in vec3 aCol;
 
 uniform mat4 Model;
 uniform mat4 View;
@@ -10,7 +9,6 @@ uniform mat4 Projection;
 
 out vec3 vertexColour;
 void main(){
-	/*vertexColour = aCol ;*/
 	gl_Position = Projection * View * Model * vec4(aPos, 1.0) ;
 
 }
